85c1d228f6203bfb8cfd63c3c01da1a62812a5d8
Lauren Shockley <131488577+laurentash@users.noreply.github.com>
2023-06-30 15:49:59 -0400

    Picture Adds!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,54 +4,76 @@
 
     
 <section class="bg-black">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-        
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight font-serif leading-none text-white md:text-5xl lg:text-6xl">Your one-stop-shop for all things me</h1>
-        <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
-        
-        <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-            <a href="#" class="inline-flex justify-center items-center text-[#e5ff00] py-3 px-5 text-base font-medium text-center hover:text-white rounded-lg bg-primary-700 focus:ring-4 focus:ring-primary-300">
-                Learn more
-                <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </a>
-            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                <svg class="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
-                Watch video
-            </a>
+    <div class="px-4 py-14 lg:py-16 lg:px-12">
+        <div class="mx-auto max-w-screen-xl text-center ">
+            
+            <h1 class="mb-4 text-4xl font-extrabold tracking-tight font-serif leading-none text-white md:text-5xl lg:text-6xl">Your one-stop-shop for all things me</h1>
+            <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Not only do you get to visit a cool website, you get to learn about me in the process!</p>
+            
+            <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+                <a href="#" class="inline-flex justify-center items-center text-[#e5ff00] py-3 px-5 text-base font-medium text-center hover:text-white rounded-lg bg-primary-700 focus:ring-4 focus:ring-primary-300">
+                    Resume
+                    <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
+                <a href="#" class="inline-flex justify-center items-center text-[#e5ff00] py-3 px-5 text-base font-medium text-center hover:text-white rounded-lg bg-primary-700 focus:ring-4 focus:ring-primary-300">
+                    About Page
+                    <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
+            </div>
         </div>
     </div>
-
-    <div class="max-w-screen-lg mx-auto text-center">
-        <dl class="grid max-w-screen-md gap-8 mx-auto text-white sm:grid-cols-3">
-            <div class="flex flex-col items-center justify-center">
-                <dt class="mb-2 text-3xl md:text-4xl font-extrabold">6YRs+</dt>
-                <dd class="font-light text-white">working experience</dd>
-            </div>
-            <div class="flex flex-col items-center justify-center">
-                <dt class="mb-2 text-3xl md:text-4xl font-extrabold">20+</dt>
-                <dd class="font-light text-white">technical skills</dd>
-            </div>
-            <div class="flex flex-col items-center justify-center">
-                <dt class="mb-2 text-3xl md:text-4xl font-extrabold">100%</dt>
-                <dd class="font-light text-white">satisfaction</dd>
-            </div>
-        </dl>
-    </div>
+        
 </section>
     
-<h2 class="text-white text-3xl text-center text-bold font-serif px-4 py-15">sapere aude<h2>
-<p class="text-sm text-gray-500 italic text-center">dare to know</p>
 
 
 
+<div class="px-8 py-18"
+    ​<section class="bg-black">
+
+        <h2 class="text-white text-4xl text-center text-bold font-serif px-4 py-15">Sapere Aude<h2>
+        <p class="text-sm text-gray-500 italic text-center">dare to know</p>
+            
+        <div class="gap-8 items-center py-4 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-8 lg:px-6">
+            <img class="w-full transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0 rounded-lg shadow-lg" src="sitting.jpeg"/>
+            <div class="mt-4 md:mt-0">
+                <h2 class="mb-4 text-4xl font-serif tracking-tight font-semibold text-white">Someone You Can Rely On</h2>
+                <p class="mb-6 font-light text-gray-300 md:text-lg">We're the Wreck Techs! We are a team of undergraduate students at Georgia Tech that assist other students, faculty, and staff throughout the year with technical and customer service needs, specializing in fixing wired and wireless Internet issues within campus residential environments. We assist customers via email, phone, live chat, and in walk-up locations in both Howell Residence Hall and West Village Dining Commons.</p>
+            </div>
+        </div>
+    </section>
+
+    ​<section class="bg-black">
+        <div class="gap-8 items-center py-4 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-8 lg:px-6">
+            {{-- <img class="w-md transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0 rounded-lg shadow-lg" src="sitting.jpeg"/>
+            <div class="mt-4 md:mt-0"> --}}
+                
+        
+                <div class="col-span-1">
+                    <h2 class="mb-4 text-4xl font-serif tracking-tight font-semibold text-white">Person over Production</h2>
+                    <p class="mb-6 font-light text-gray-300 md:text-lg">We're the Wreck Techs! We are a team of undergraduate students at Georgia Tech that assist other students, faculty, and staff throughout the year with technical and customer service needs, specializing in fixing wired and wireless Internet issues within campus residential environments. We assist customers via email, phone, live chat, and in walk-up locations in both Howell Residence Hall and West Village Dining Commons.</p>
+                </div>
+    
+                <div id="custom-controls-gallery" class="relative w-full" data-carousel="slide">
+                    <!-- Carousel wrapper -->
+                    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                        <!-- Item 1 -->
+                        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                            <img src="bench.jpg" class="absolute w-full transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0 rounded-lg shadow-lg block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="Park Bench">
+                        </div>
+                        <!-- Item 2 -->
+                        <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
+                            <img src="beach.jpg" class="w-full transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0 rounded-lg shadow-lg absolute block max-w-full h-auto -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="Beach">
+                        </div>
+                    </div>
+                    
+                </div>
 
 
-
-
-
-<img class="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Rounded avatar">
-<img class="w-10 h-10 rounded" src="/docs/images/people/profile-picture-5.jpg" alt="Default avatar">
-// BLURRRRRRR TO PICTURES 
+            </div>
+        </div>
+    </section>
+</div>
 
 
 @endsection
