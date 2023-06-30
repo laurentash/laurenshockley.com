7b1d31a31650a9cbb73999c50113857486b1eabf
Lauren Shockley <131488577+laurentash@users.noreply.github.com>
2023-06-30 16:27:29 -0400

    content add

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -2,4 +2,8 @@
 
 @section('content')
 
+<div class="py-16">
+    <h1 class="text-center text-4xl font-serif font-extrabold text-white">Thank you for submitting your information!</h1>
+    <p class="text-center text-gray-300 font-normal">See the complete resume below.</p>
+</div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,11 +11,11 @@
             <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Not only do you get to visit a cool website, you get to learn about me in the process!</p>
             
             <div class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                <a href="#" class="inline-flex justify-center items-center text-[#e5ff00] py-3 px-5 text-base font-medium text-center hover:text-white rounded-lg bg-primary-700 focus:ring-4 focus:ring-primary-300">
+                <a href="/resume" class="inline-flex justify-center items-center text-[#e5ff00] py-3 px-5 text-base font-medium text-center hover:text-[#E384FF] rounded-lg">
                     Resume
                     <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </a>
-                <a href="#" class="inline-flex justify-center items-center text-[#e5ff00] py-3 px-5 text-base font-medium text-center hover:text-white rounded-lg bg-primary-700 focus:ring-4 focus:ring-primary-300">
+                <a href="/about" class="inline-flex justify-center items-center text-[#e5ff00] py-3 px-5 text-base font-medium text-center hover:text-[#E384FF] rounded-lg">
                     About Page
                     <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </a>
@@ -50,7 +50,7 @@
                 
         
                 <div class="col-span-1">
-                    <h2 class="mb-4 text-4xl font-serif tracking-tight font-semibold text-white">Person over Production</h2>
+                    <h2 class="mb-4 text-4xl font-serif tracking-tight font-semibold text-white">Person over Payables. Symbiosis over Settlement </h2>
                     <p class="mb-6 font-light text-gray-300 md:text-lg">We're the Wreck Techs! We are a team of undergraduate students at Georgia Tech that assist other students, faculty, and staff throughout the year with technical and customer service needs, specializing in fixing wired and wireless Internet issues within campus residential environments. We assist customers via email, phone, live chat, and in walk-up locations in both Howell Residence Hall and West Village Dining Commons.</p>
                 </div>
     
